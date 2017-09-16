b75462deccbb80f29017d7e08ad3b53a059f67c5
payneTsai <30742937+payneTsai@users.noreply.github.com>
2017-09-16 21:02:40 +0800

    修改db.propeties文件的用户名跟密码

--- src/test/java/com/youmeek/ssm/SSMTest.java
+++ src/test/java/com/youmeek/ssm/SSMTest.java
@@ -4,10 +4,16 @@ import com.youmeek.ssm.module.user.pojo.SysUser;
 import com.youmeek.ssm.module.user.service.SysUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import sun.net.ApplicationProxy;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:spring/applicationContext*.xml"})
@@ -22,4 +28,7 @@ public class SSMTest {
 		SysUser sysUser = sysUserService.getById(1L);
 		System.out.println("--------------------------------" + sysUser.toString());
 	}
+
+
+
 }
